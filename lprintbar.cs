4c4aeede8d20ba70fc5a5b83d01b87bc64f9765b
VlexUIA <xfalkon61@gmail.com>
2026-04-12 16:31:16 +0200

    lprintbar.cs (Ferdig)

---
+++ lprintbar.cs
@@ -0,0 +1,8 @@
+namespace IS110OBLIG1
+{
+    // Interface - en "kontrakt" som sier at alle klasser som bruker dett må ha en PrintInfo()
+    interface IPrintbar
+    {
+        void PrintInfo();
+    }
+}

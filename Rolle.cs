17f20a45eab07eb53ac679221ee9b89b56c1ca95
VlexUIA <xfalkon61@gmail.com>
2026-04-12 16:30:39 +0200

    Rolle.cs (Ferdig)

---
+++ Rolle.cs
@@ -0,0 +1,11 @@
+namespace IS110OBLIG1
+{
+    // Enum - en liste med faste verdier som beskriver hvilken rolle en bruker har
+    // Brukes i stedet for å sammenligne tekst som "student" eller "faglærer"
+    enum Rolle
+    {
+        Student,
+        Faglarer,
+        Bibliotekansatt
+    }
+}
